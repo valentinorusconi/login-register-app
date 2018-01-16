2ea1077122aab0f9ea24dd6ec5df827c378222c2
Valentino Rusconi <vaekek3@gmail.com>
2018-01-16 17:09:17 +0100

    Delete main.js

--- server/config/main.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  //secret key for JWT singing and encryption
-  "secret" : "super secret passphrase",
-  "database" : "mongodb://admin:admin@ds159187.mlab.com:59187/login-register-app",
-  'port' : process.env.PORT || 5000
-}
